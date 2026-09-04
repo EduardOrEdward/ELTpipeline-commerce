f66154d781c1b0e2d1df2fb4a46ae8ad49dcf147
BenkeEdward <eduardbenke01@gmail.com>
2026-09-04 21:08:00 +0200

    made APIRouter - decorator

--- mock_api/models/deliveries.py
+++ mock_api/models/deliveries.py
@@ -2,7 +2,7 @@
 # DATA -> delivery_id, order_id, actual_quanity, actual_delivery_date
 from datetime import date
 from pydantic import BaseModel, Field
-from routers.orders import *
+from mock_api.models.orders import Order
 class Delivery(BaseModel):
     delivery_id:int=Field(...,description="The ID of our order")
     
--- mock_api/routers/orders.py
+++ mock_api/routers/orders.py
@@ -1,5 +1,15 @@
 ## ORDERS
 # We need to make syntethic of order_id, product_id, supplier_id, planned_quanity, order_date, excepted_delivery_date
-from fastapi import APIRouter
+from fastapi import APIRouter,Request
 import faker
+from typing import Any, Optional,Dict
+import logging,time
+from mock_api.models.orders import Order
+logger = logging.getLogger(__name__)
 
+router = APIRouter(tags=["Orders"])
+
+@router.post("/make_order",response_model=Order,summary="Sending request to make a order")
+def make_order(request:Request,) -> Dict[Order]:
+    start = time.perf_counter()
+    pass
